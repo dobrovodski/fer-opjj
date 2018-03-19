081112ae0e67b32283eb2a6de0d1684ae0b6efb5
Matej Dobrovodski <matej.dobrovodski@fer.hr>
2018-03-20 00:16:30 +0100

    Task one done (without documentation)

---
+++ hw02/hw02-0036491377/src/main/java/hr/fer/zemris/java/custom/collections/Collection.java
@@ -0,0 +1,52 @@
+package hr.fer.zemris.java.custom.collections;
+
+public class Collection {
+
+	protected Collection() {
+
+	}
+
+	public boolean isEmpty() {
+		return size() > 0;
+	}
+
+	public int size() {
+		return 0;
+	}
+
+	public void add(Object value) {
+
+	}
+
+	boolean contains(Object value) {
+		return false;
+	}
+
+	boolean remove(Object value) {
+		return false;
+	}
+
+	Object[] toArray() {
+		throw new UnsupportedOperationException();
+	}
+
+	void forEach(Processor processor) {
+
+	}
+
+	void addAll(Collection other) {
+		class LocalProcessor extends Processor {
+			public void process(Object value) {
+				add(value);
+			}
+		}
+		
+		LocalProcessor processor = new LocalProcessor();
+		other.forEach(processor);
+	}
+
+	void clear() {
+		
+	}
+
+}
