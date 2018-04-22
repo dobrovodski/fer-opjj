303a8d4c0ed6dfc0dfe62ac63ddd800ee36fd79c
Matej Dobrovodski <matej.dobrovodski@fer.hr>
2018-04-22 17:21:17 +0200

    Crypto works but is buggy and unpolished

--- hw07/hw07-0036491377/src/main/java/hr/fer/zemris/java/hw06/crypto/Crypto.java
+++ hw07/hw07-0036491377/src/main/java/hr/fer/zemris/java/hw06/crypto/Crypto.java
@@ -1,18 +1,25 @@
 package hr.fer.zemris.java.hw06.crypto;
 
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.AlgorithmParameterSpec;
 import java.util.Scanner;
 
 import static hr.fer.zemris.java.hw06.crypto.Util.bytetohex;
 
 public class Crypto {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         if (args.length < 2) {
             System.out.println("NO :<");
         }
@@ -22,13 +29,47 @@ public class Crypto {
         Scanner sc = new Scanner(System.in);
 
         switch (type) {
-            case CHECKSHA:
-                System.out.print("Please provide expected sha-256 digest for hw06part2.pdf: \n> ");
+            case CHECKSHA: {
+                System.out.format("Please provide expected sha-256 digest for %s: %n> ", path);
                 String provided = sc.nextLine();
                 checkSHA(path, provided);
+            }
             case ENCRYPT:
-                break;
+                if (args.length < 3) {
+                    System.out.println("Encryption requires an input and an output file.");
+                    return;
+                }
+
             case DECRYPT:
+                if (args.length < 3) {
+                    System.out.println("Decryption requires an input and an output file.");
+                    return;
+                }
+
+                Path out = Paths.get(args[2]);
+                System.out.format("Please provide password as hex-encoded text (16 bytes, i.e. 32 hex-digits): %n> ");
+                String password = sc.nextLine();
+                System.out.format("Please provide initialization vector as hex-encoded text (32 hex-digits): %n> ");
+                String initVector = sc.nextLine();
+
+                SecretKeySpec keySpec = new SecretKeySpec(Util.hextobyte(password), "AES");
+                AlgorithmParameterSpec paramSpec = new IvParameterSpec(Util.hextobyte(initVector));
+                Cipher cipher;
+                try {
+                    cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+                    System.out.println("Could not instantiate cipher.");
+                    return;
+                }
+
+                try {
+                    cipher.init(false ? Cipher.ENCRYPT_MODE : Cipher.DECRYPT_MODE, keySpec, paramSpec);
+                } catch (InvalidKeyException | InvalidAlgorithmParameterException e) {
+                    System.out.println("Could not initialize cipher.");
+                    return;
+                }
+
+                encryptDecrypt(path, out, cipher);
                 break;
         }
 
@@ -73,6 +114,24 @@ public class Crypto {
             System.out.println("matches expected digest.");
         } else {
             System.out.format("does not match the expected digest. Digest was: %s%n", digest);
+        }
+    }
+
+    private static void encryptDecrypt(Path in, Path out, Cipher cipher) {
+        byte[] bytes = new byte[4096];
+        byte[] output;
+
+        try (InputStream is = new BufferedInputStream(Files.newInputStream(in, StandardOpenOption.READ));
+             OutputStream os = new BufferedOutputStream(Files.newOutputStream(out, StandardOpenOption.CREATE))
+        ) {
+            int count;
+            while ((count = is.read(bytes)) != -1) {
+                output = cipher.update(bytes, 0, count);
+                os.write(output);
+            }
+        } catch (IOException ignored) {
+            //TODO
+            return;
         }
     }
 }
