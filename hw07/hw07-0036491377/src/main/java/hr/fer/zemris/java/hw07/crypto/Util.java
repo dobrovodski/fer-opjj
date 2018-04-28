bf2997bcf514e3e64df9f6fb3da3f50d46c8fddd
Matej Dobrovodski <matej.dobrovodski@fer.hr>
2018-04-28 16:05:43 +0200

    Stop relying on Integer and Character methods in Util

--- hw07/hw07-0036491377/src/main/java/hr/fer/zemris/java/hw07/crypto/Util.java
+++ hw07/hw07-0036491377/src/main/java/hr/fer/zemris/java/hw07/crypto/Util.java
@@ -15,6 +15,8 @@ public class Util {
      */
     public static byte[] hextobyte(String keyText) {
         int length = keyText.length();
+        keyText = keyText.toLowerCase();
+
         if (length % 2 != 0) {
             throw new IllegalArgumentException("Hex cannot have an odd length.");
         }
@@ -23,8 +25,9 @@ public class Util {
         for (int i = 0; i < length; i += 2) {
             char c1 = keyText.charAt(i);
             char c2 = keyText.charAt(i + 1);
-            int b1 = Character.digit(c1, 16) * 16;
-            int b2 = Character.digit(c2, 16);
+
+            int b1 = hexDigitToNumber(c1) * 16;
+            int b2 = hexDigitToNumber(c2);
 
             if (b1 == -1 || b2 == -1) {
                 throw new IllegalArgumentException("Could not convert to byte: " + c1 + c2);
@@ -37,6 +40,30 @@ public class Util {
     }
 
     /**
+     * Converts given hexadecimal digit (0-9 and A-F) to the appropriate numerical value. If the digit is invalid,
+     * returns -1.
+     *
+     * @param c digit to convert
+     *
+     * @return converted value or -1 if it is an invalid character
+     */
+    private static int hexDigitToNumber(char c) {
+        if (c >= 48 && c <= 57) {
+            return c - 48;
+        }
+
+        if (c >= 97 && c <= 102) {
+            return c - 87;
+        }
+
+        if (c >= 65 && c <= 70) {
+            return c - 55;
+        }
+
+        return -1;
+    }
+
+    /**
      * Converts given array of bytes into a hex string.
      *
      * @param bytearray array of bytes to convert
@@ -45,14 +72,13 @@ public class Util {
      */
     public static String bytetohex(byte[] bytearray) {
         StringBuilder sb = new StringBuilder();
-        for (byte byteNum : bytearray) {
-            //TODO: write own tohexstring?
-            String converted = Integer.toHexString(byteNum);
-            if (converted.length() < 2) {
-                converted = '0' + converted;
-            }
+        char[] charMap = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
-            sb.append(converted);
+        for (byte byteNum : bytearray) {
+            char c1 = (char) ((byteNum >> 4) & 0xF);
+            char c2 = (char) ((byteNum) & 0xF);
+
+            sb.append(charMap[c1]).append(charMap[c2]);
         }
 
         return sb.toString();
