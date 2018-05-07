dc421cc53e0cfc29c4f9462193b6417b69c7e455
Matej Dobrovodski <matej.dobrovodski@fer.hr>
2018-05-07 23:19:53 +0200

    Everything polynomial related but toComplexPolynomial is implemented now

--- hw09/hw09-0036491377/lib/hr/fer/zemris/math/ComplexPolynomial.java
+++ hw09/hw09-0036491377/lib/hr/fer/zemris/math/ComplexPolynomial.java
@@ -1,14 +1,14 @@
 package hr.fer.zemris.math;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Objects;
 
 public class ComplexPolynomial {
     private Complex[] factors;
 
 
     public ComplexPolynomial(Complex... factors) {
+        Objects.requireNonNull(factors, "Factors cannot be null.");
         this.factors = factors;
     }
 
@@ -17,15 +17,17 @@ public class ComplexPolynomial {
     }
 
     public ComplexPolynomial multiply(ComplexPolynomial p) {
-        List<Complex> newFactors = new ArrayList<>();
+        Objects.requireNonNull(p, "Cannot multiply with null.");
+        Complex[] newFactors = new Complex[factors.length];
 
-        for (Complex factor : factors) {
-            for (Complex other : p.factors) {
-                newFactors.add(factor.multiply(other));
+        for (int i = 0; i < factors.length; i++) {
+            for (int j = 0; j < p.factors.length; j++) {
+                int index = i * j;
+                Complex mul = factors[i].multiply(p.factors[j]);
+                newFactors[index] = newFactors[index] == null ? mul : newFactors[index].add(mul);
             }
         }
-
-        return new ComplexPolynomial(newFactors.toArray(new Complex[0]));
+        return new ComplexPolynomial(newFactors);
     }
 
     public ComplexPolynomial derive() {
@@ -43,11 +45,24 @@ public class ComplexPolynomial {
     }
 
     public Complex apply(Complex z) {
-        return null;
+        Objects.requireNonNull(z, "Cannot apply polynomial to null.");
+        Complex sum = new Complex(0, 0);
+        for (Complex factor : factors) {
+            sum = sum.add(z.power(0).multiply(factor));
+        }
+
+        return sum;
     }
 
     @Override
     public String toString() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < factors.length; i++) {
+            Complex c = factors[i];
+            String exp = i > 0 ? "z^" + i : "";
+            sb.append(String.format("(%s)%s", c.toString(), exp));
+        }
+
+        return sb.toString();
     }
 }
--- hw09/hw09-0036491377/lib/hr/fer/zemris/math/ComplexRootedPolynomial.java
+++ hw09/hw09-0036491377/lib/hr/fer/zemris/math/ComplexRootedPolynomial.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class ComplexRootedPolynomial {
-    Complex[] roots;
+    private Complex[] roots;
 
     public ComplexRootedPolynomial(Complex ...roots) {
         Objects.requireNonNull(roots, "Roots cannot be null.");
