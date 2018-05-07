171140bd7d95d5361da17056489078920879943f
Matej Dobrovodski <matej.dobrovodski@fer.hr>
2018-05-07 09:38:35 +0200

    Vector3

---
+++ hw09/hw09-0036491377/lib/hr/fer/zemris/math/Vector3.java
@@ -0,0 +1,70 @@
+package hr.fer.zemris.math;
+
+public class Vector3 {
+    double x;
+    double y;
+    double z;
+
+    public Vector3(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double norm() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public Vector3 normalized() {
+        double norm = norm();
+        return new Vector3(x / norm, y / norm, z / norm);
+    }
+
+    public Vector3 add(Vector3 other) {
+        return new Vector3(x + other.x, y + other.y, z + other.z);
+    }
+
+    public Vector3 sub(Vector3 other) {
+        return new Vector3(x - other.x, y - other.y, z - other.z);
+    }
+
+    public double dot(Vector3 other) {
+        return x * other.x + y * other.y + z * other.z;
+    }
+
+    public Vector3 cross(Vector3 other) {
+        return new Vector3(
+                y * other.z - z * other.y,
+                z * other.x - x * other.z,
+                x * other.y - y * other.x
+        );
+    }
+
+    public Vector3 scale(double s) {
+        return new Vector3(x * s, y * s, z * s);
+    }
+
+    public double cosAngle(Vector3 other) {
+        return dot(other) / (norm() * other.norm());
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public double[] toArray() {
+        return new double[]{x, y, z};
+    }
+
+    public String toString() {
+        return String.format("(%f, %f, %f)", x, y, z);
+    }
+}
