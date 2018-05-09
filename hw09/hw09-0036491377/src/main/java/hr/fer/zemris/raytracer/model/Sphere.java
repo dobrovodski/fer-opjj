b1f75163149e90d97cb55d9a8098f089e0960421
Matej Dobrovodski <matej.dobrovodski@fer.hr>
2018-05-09 15:31:04 +0200

    Raycaster skeleton

---
+++ hw09/hw09-0036491377/src/main/java/hr/fer/zemris/raytracer/model/Sphere.java
@@ -0,0 +1,36 @@
+package hr.fer.zemris.raytracer.model;
+
+import hr.fer.zemris.java.raytracer.model.GraphicalObject;
+import hr.fer.zemris.java.raytracer.model.Point3D;
+import hr.fer.zemris.java.raytracer.model.Ray;
+import hr.fer.zemris.java.raytracer.model.RayIntersection;
+
+public class Sphere extends GraphicalObject{
+    private Point3D center;
+    private double radius;
+    private double kdr;
+    private double kdg;
+    private double kdb;
+    private double krr;
+    private double krg;
+    private double krb;
+    private double krn;
+
+    public Sphere(Point3D center, double radius, double kdr, double kdg, double kdb, double krr, double krg, double
+            krb, double krn) {
+        this.center = center;
+        this.radius = radius;
+        this.kdr = kdr;
+        this.kdg = kdg;
+        this.kdb = kdb;
+        this.krr = krr;
+        this.krg = krg;
+        this.krb = krb;
+        this.krn = krn;
+    }
+
+    @Override
+    public RayIntersection findClosestRayIntersection(Ray ray) {
+        return null;
+    }
+}
