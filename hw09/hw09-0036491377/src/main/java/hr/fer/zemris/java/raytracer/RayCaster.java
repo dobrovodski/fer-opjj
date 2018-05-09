4f4d5585e1e261f0d0e571f36c635dc535413c73
Matej Dobrovodski <matej.dobrovodski@fer.hr>
2018-05-09 20:50:19 +0200

    Implement Sphere and bits of RayCaster

--- hw09/hw09-0036491377/src/main/java/hr/fer/zemris/java/raytracer/RayCaster.java
+++ hw09/hw09-0036491377/src/main/java/hr/fer/zemris/java/raytracer/RayCaster.java
@@ -1,56 +1,85 @@
 package hr.fer.zemris.java.raytracer;
 
+import hr.fer.zemris.java.raytracer.model.GraphicalObject;
 import hr.fer.zemris.java.raytracer.model.IRayTracerProducer;
 import hr.fer.zemris.java.raytracer.model.IRayTracerResultObserver;
 import hr.fer.zemris.java.raytracer.model.Point3D;
 import hr.fer.zemris.java.raytracer.model.Ray;
+import hr.fer.zemris.java.raytracer.model.RayIntersection;
 import hr.fer.zemris.java.raytracer.model.Scene;
 import hr.fer.zemris.java.raytracer.viewer.RayTracerViewer;
 
 public class RayCaster {
     public static void main(String[] args) {
         RayTracerViewer.show(getIRayTracerProducer(),
-                new Point3D(10,0,0),
-                new Point3D(0,0,0),
-                new Point3D(0,0,10),
+                new Point3D(10, 0, 0),
+                new Point3D(0, 0, 0),
+                new Point3D(0, 0, 10),
                 20, 20);
     }
+
     private static IRayTracerProducer getIRayTracerProducer() {
-        return new IRayTracerProducer() {
-            @Override
-            public void produce(Point3D eye, Point3D view, Point3D viewUp,
-                    double horizontal, double vertical, int width, int height,
-                    long requestNo, IRayTracerResultObserver observer) {
-                System.out.println("Započinjem izračune...");
-                short[] red = new short[width*height];
-                short[] green = new short[width*height];
-                short[] blue = new short[width*height];
-                Point3D zAxis = new Point3D(0, 0, 1);
-                Point3D yAxis = new Point3D(0, 1, 0);
-                Point3D xAxis = new Point3D(1, 0, 0);
-                Point3D screenCorner = new Point3D(1, 1, 0);
-                Scene scene = RayTracerViewer.createPredefinedScene();
-                short[] rgb = new short[3];
-                int offset = 0;
-                for(int y = 0; y < height; y++) {
-                    for(int x = 0; x < width; x++) {
-                        Point3D screenPoint = new Point3D(1.0 * x / width, 1.0 * y / height, 1);
-                        Ray ray = Ray.fromPoints(eye, screenPoint);
-                        tracer(scene, ray, rgb);
-                        red[offset] = rgb[0] > 255 ? 255 : rgb[0];
-                        green[offset] = rgb[1] > 255 ? 255 : rgb[1];
-                        blue[offset] = rgb[2] > 255 ? 255 : rgb[2];
-                        offset++;
-                    }
+        return (eye, view, viewUp, horizontal, vertical, width, height, requestNo, observer) -> {
+            System.out.println("Započinjem izračune...");
+
+            short[] red = new short[width * height];
+            short[] green = new short[width * height];
+            short[] blue = new short[width * height];
+
+            Point3D zAxis = new Point3D(0, 0, 1);
+            Point3D yAxis = new Point3D(0, 1, 0);
+            Point3D xAxis = new Point3D(1, 0, 0);
+            Point3D screenCorner = new Point3D(1, 1, 0);
+            Scene scene = RayTracerViewer.createPredefinedScene();
+
+            short[] rgb = new short[3];
+            int offset = 0;
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    Point3D screenPoint = new Point3D(1.0 * x / width, 1.0 * y / height, 1);
+                    Ray ray = Ray.fromPoints(eye, screenPoint);
+                    tracer(scene, ray, rgb);
+                    red[offset] = rgb[0] > 255 ? 255 : rgb[0];
+                    green[offset] = rgb[1] > 255 ? 255 : rgb[1];
+                    blue[offset] = rgb[2] > 255 ? 255 : rgb[2];
+                    offset++;
                 }
-                System.out.println("Izračuni gotovi...");
-                observer.acceptResult(red, green, blue, requestNo);
-                System.out.println("Dojava gotova...");
             }
+
+            System.out.println("Izračuni gotovi...");
+            observer.acceptResult(red, green, blue, requestNo);
+            System.out.println("Dojava gotova...");
         };
     }
 
     private static void tracer(Scene scene, Ray ray, short[] rgb) {
+        rgb[0] = 0;
+        rgb[1] = 0;
+        rgb[2] = 0;
+        RayIntersection closest = findClosestIntersection(scene, ray);
+        if (closest == null) {
+            return;
+        }
+        rgb[0] = 255;
+        rgb[1] = 255;
+        rgb[2] = 255;
+    }
 
+    private static RayIntersection findClosestIntersection(Scene scene, Ray ray) {
+        RayIntersection closest = null;
+
+        for (GraphicalObject obj : scene.getObjects()) {
+            RayIntersection current = obj.findClosestRayIntersection(ray);
+
+            if (current == null) {
+                continue;
+            }
+
+            if (closest == null || current.getDistance() < closest.getDistance()) {
+                closest = current;
+            }
+        }
+
+        return closest;
     }
 }
---
+++ hw09/hw09-0036491377/src/main/java/hr/fer/zemris/java/raytracer/model/Sphere.java
@@ -0,0 +1,132 @@
+package hr.fer.zemris.java.raytracer.model;
+
+import java.util.Objects;
+
+/**
+ * This class represents a sphere and provides the findClosestRayIntersection to find the closest intersection between a
+ * ray and the sphere.
+ *
+ * @author matej
+ */
+public class Sphere extends GraphicalObject {
+    private Point3D center;
+    private double radius;
+    private double kdr;
+    private double kdg;
+    private double kdb;
+    private double krr;
+    private double krg;
+    private double krb;
+    private double krn;
+
+    /**
+     * The constructor for {@link Sphere}.
+     *
+     * @param center center of the sphere
+     * @param radius radius of the sphere
+     * @param kdr diffuse component for the color red
+     * @param kdg diffuse component for the color green
+     * @param kdb diffuse component for the color blue
+     * @param krr reflection component for the color red
+     * @param krg reflection component for the color green
+     * @param krb reflection component for the color blue
+     * @param krn reflection component
+     */
+    public Sphere(Point3D center, double radius, double kdr, double kdg, double kdb, double krr, double krg, double
+            krb, double krn) {
+        Objects.requireNonNull(center, "Center cannot be null.");
+
+        this.center = center;
+        this.radius = radius;
+        this.kdr = kdr;
+        this.kdg = kdg;
+        this.kdb = kdb;
+        this.krr = krr;
+        this.krg = krg;
+        this.krb = krb;
+        this.krn = krn;
+    }
+
+    @Override
+    public RayIntersection findClosestRayIntersection(Ray ray) {
+        Objects.requireNonNull(ray, "Ray cannot be null");
+
+        // ||x-c||^2 = r^2 - sphere
+        // x = o + dl - ray
+        // o, l, c, x are vectors
+        // ||o + dl - c||^2 = r^2
+        // d^2*l*l + 2*d*l*(o-c) + (o-c)^2 = r^2
+        // ad^2 + bd + c = 0
+
+        Point3D OC = ray.start.sub(center);
+
+        double a = ray.direction.scalarProduct(ray.direction);
+        double b = 2 * ray.direction.scalarProduct(OC);
+        double c = OC.scalarProduct(OC) - radius * radius;
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant < 0) {
+            return null;
+        }
+
+        double discSqrt = Math.sqrt(discriminant);
+        double x1 = (-b + discSqrt) / (2 * a);
+        double x2 = (-b - discSqrt) / (2 * a);
+
+        double x = x1 < x2 ? x1 : x2;
+        Point3D intersectionPoint = ray.start.add(ray.direction.scalarMultiply(x));
+        return new RayIntersectionImpl(
+                intersectionPoint,
+                x,
+                ray.start.sub(center).norm() < ray.start.sub(intersectionPoint).norm()
+        );
+    }
+
+    private class RayIntersectionImpl extends RayIntersection {
+
+        RayIntersectionImpl(Point3D point, double distance, boolean outer) {
+            super(point, distance, outer);
+        }
+
+        @Override
+        public Point3D getNormal() {
+            return getPoint().sub(center).normalize();
+        }
+
+        @Override
+        public double getKdr() {
+            return kdr;
+        }
+
+        @Override
+        public double getKdg() {
+            return kdg;
+        }
+
+        @Override
+        public double getKdb() {
+            return kdb;
+        }
+
+        @Override
+        public double getKrr() {
+            return krr;
+        }
+
+        @Override
+        public double getKrg() {
+            return krg;
+        }
+
+        @Override
+        public double getKrb() {
+            return krb;
+        }
+
+        @Override
+        public double getKrn() {
+            return krn;
+        }
+    }
+}
