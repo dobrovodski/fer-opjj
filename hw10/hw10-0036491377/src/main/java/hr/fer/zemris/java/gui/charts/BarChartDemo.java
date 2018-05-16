f9d734481dffddf50af98cbe1807c7a4a52c036c
Matej Dobrovodski <matej.dobrovodski@fer.hr>
2018-05-17 01:14:35 +0200

    Delete Demo and make BarChartDemo work

--- hw10/hw10-0036491377/src/main/java/hr/fer/zemris/java/gui/charts/BarChartDemo.java
+++ hw10/hw10-0036491377/src/main/java/hr/fer/zemris/java/gui/charts/BarChartDemo.java
@@ -2,43 +2,53 @@ package hr.fer.zemris.java.gui.charts;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BarChartDemo extends JFrame {
+    private static List<XYValue> values;
+    private static int minY;
+    private static int maxY;
+    private static int gapY;
+    private static String xDescription;
+    private static String yDescription;
 
     public BarChartDemo() {
         setLocation(20, 50);
-        setSize(300, 200);
+        setSize(600, 600);
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         initGUI();
     }
 
     public static void main(String[] args) {
-        /* if (args.length != 1) {
+        if (args.length != 1) {
             System.out.println("Program requires one argument.");
             return;
         }
 
         String fileName = args[0];
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            String xDescription = br.readLine();
-            String yDescription = br.readLine();
+            xDescription = br.readLine();
+            yDescription = br.readLine();
 
             String[] dataStr = br.readLine().replaceAll("\\s+", " ").split(" ");
-            List<XYValue> values = parseData(dataStr);
+            values = parseData(dataStr);
 
-            int minY = Integer.parseInt(br.readLine());
-            int maxY = Integer.parseInt(br.readLine());
+            minY = Integer.parseInt(br.readLine());
+            maxY = Integer.parseInt(br.readLine());
 
-            int gapY = Integer.parseInt(br.readLine());
+            gapY = Integer.parseInt(br.readLine());
 
-            new BarChart(values, xDescription, yDescription, minY, maxY, gapY);
         } catch (IOException e) {
             System.out.println("File could not be read: " + fileName);
+            return;
         } catch (NumberFormatException e) {
             System.out.println("Chart attributes must be whole numbers.");
-        } */
+            return;
+        }
 
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new BarChartDemo();
@@ -59,6 +69,13 @@ public class BarChartDemo extends JFrame {
 
     private void initGUI() {
         Container cp = getContentPane();
-        //cp.add(new BarChartComponent(new BarChart()));
+        cp.add(new BarChartComponent(new BarChart(
+                values,
+                xDescription,
+                yDescription,
+                minY,
+                maxY,
+                gapY
+        )));
     }
 }
