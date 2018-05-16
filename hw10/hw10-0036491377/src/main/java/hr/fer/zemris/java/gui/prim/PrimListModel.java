4e6b7b5b9ccb42352350039e80611d578875841a
Matej Dobrovodski <matej.dobrovodski@fer.hr>
2018-05-17 01:56:12 +0200

    Rearrange code

--- hw10/hw10-0036491377/src/main/java/hr/fer/zemris/java/gui/prim/PrimListModel.java
+++ hw10/hw10-0036491377/src/main/java/hr/fer/zemris/java/gui/prim/PrimListModel.java
@@ -11,45 +11,6 @@ public class PrimListModel implements ListModel<Integer> {
     private List<ListDataListener> listeners;
     private int current;
 
-    public void next() {
-        while (true) {
-            if (current == 1 || current % 2 == 0) {
-                current += 1;
-            } else {
-                current += 2;
-            }
-
-            if (isPrime(current)) {
-                break;
-            }
-        }
-
-        int pos = primes.size();
-        primes.add(current);
-        ListDataEvent event = new ListDataEvent(this, ListDataEvent.INTERVAL_ADDED, pos, pos);
-        for (ListDataListener l : listeners) {
-            l.intervalAdded(event);
-        }
-    }
-
-    private boolean isPrime(int n) {
-        if (n == 2) {
-            return true;
-        }
-
-        if (n % 2 == 0) {
-            return false;
-        }
-
-        for (int i = 3; i * i <= n; i += 2) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
     public PrimListModel() {
         super();
         primes = new ArrayList<>();
@@ -76,5 +37,45 @@ public class PrimListModel implements ListModel<Integer> {
     @Override
     public void removeListDataListener(ListDataListener l) {
         listeners.remove(l);
+    }
+
+    public void next() {
+        while (true) {
+            if (current == 1 || current % 2 == 0) {
+                current += 1;
+            } else {
+                current += 2;
+            }
+
+            if (isPrime(current)) {
+                break;
+            }
+        }
+
+        int pos = primes.size();
+        primes.add(current);
+
+        ListDataEvent event = new ListDataEvent(this, ListDataEvent.INTERVAL_ADDED, pos, pos);
+        for (ListDataListener l : listeners) {
+            l.intervalAdded(event);
+        }
+    }
+
+    private boolean isPrime(int n) {
+        if (n == 2) {
+            return true;
+        }
+
+        if (n % 2 == 0) {
+            return false;
+        }
+
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
