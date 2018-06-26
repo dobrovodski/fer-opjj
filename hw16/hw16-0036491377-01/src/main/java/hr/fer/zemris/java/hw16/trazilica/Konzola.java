4a40c89ba074401f185797df6e67fbbe18a6a7f9
Matej Dobrovodski <matej.dobrovodski@fer.hr>
2018-06-26 17:04:52 +0200

    Reorganized most of the logic into SearchEngine

--- hw16/hw16-0036491377-01/src/main/java/hr/fer/zemris/java/hw16/trazilica/Konzola.java
+++ hw16/hw16-0036491377-01/src/main/java/hr/fer/zemris/java/hw16/trazilica/Konzola.java
@@ -1,220 +1,48 @@
 package hr.fer.zemris.java.hw16.trazilica;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
-import java.util.stream.Stream;
 
 public class Konzola {
     private static final String PROMPT = "Enter command > ";
-    private static final String STOPWORD_PATH = "stop_rijeci.txt";
 
-    private static Set<String> vocabulary;
-    private static Set<String> stopWords;
-    private static Map<String, Double> idf;
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 1) {
             System.out.println("This program requires only the path to a folder of text documents.");
             return;
         }
 
-        String filepath = args[0];
+        SearchEngine se;
         try {
-            stopWords = buildStopWordSet(STOPWORD_PATH);
+            se = new SearchEngine(args[0]);
         } catch (IOException e) {
-            System.out.println("Could not read stop word file: " + filepath);
+            System.out.println("Could not create search engine using documents in: " + args[0]);
             return;
         }
-
-        try {
-            vocabulary = buildVocabulary(filepath);
-        } catch (IllegalArgumentException e) {
-            System.out.println("Could not build vocabulary: " + e.getMessage());
-            return;
-        }
-
-        List<Document> documents;
-        documents = buildDocuments(filepath, stopWords);
-
-        idf = buildIdf(documents);
 
         Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.print(PROMPT);
             String[] in = sc.nextLine().trim().split(" ", 2);
             String command = in[0].trim();
+
             if (command.equals("query")) {
+                String arg = in[1].trim();
                 List<String> words = Arrays.asList(in[1].replaceAll("\\s+", " ").split(" "));
-
-                System.out.print('[');
-                for (String w : words) {
-                    System.out.print(w + ',');
-                }
-                System.out.println(']');
-
-                Document queryDoc = new Document(words, stopWords);
-                queryDoc.setIdf(idf);
-                for (Document doc : documents) {
-                    System.out.println(calculateSimilarity(doc, queryDoc) + " ~ ~ ~ " + doc.getFilepath());
-                }
+                se.calculateSimilaritiesTo(words);
+                se.getTopDocuments(10);
             } else if (command.equals("type")) {
-
+                String arg = in[1].trim();
+                se.getResults().get(Integer.parseInt(arg)).getDocument().print(System.out);
             } else if (command.equals("results")) {
-
+                se.getTopDocuments(10);
             } else if (command.equals("exit")) {
                 break;
             } else {
                 System.out.println("Nepoznata naredba.");
             }
         }
-    }
-
-    private static Set<String> buildVocabulary(String filepath) {
-        Path path = Paths.get(filepath);
-        Stream<Path> filePathStream;
-        Set<String> vocabulary = new TreeSet<>();
-
-        try {
-            filePathStream = Files.walk(path);
-        } catch (IOException e) {
-            throw new IllegalArgumentException("Could not walk path " + path);
-        }
-
-        filePathStream.forEach(p -> {
-            if (Files.isRegularFile(p)) {
-                try {
-                    vocabulary.addAll(extractWords(p));
-                } catch (IOException e) {
-                    System.out.println("Could not read file: " + p.toString());
-                    System.out.println("Going to next file.");
-                }
-            }
-        });
-
-        return vocabulary;
-    }
-
-    private static Set<String> extractWords(Path path) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path.toString()), "UTF-8"));
-        Set<String> wordSet = new TreeSet<>();
-
-        String line;
-        while ((line = br.readLine()) != null) {
-            String[] words = line.trim().split("[^\\p{L}]+");
-
-            for (String word : words) {
-                word = word.trim().toLowerCase();
-                if (!word.isEmpty() && !stopWords.contains(word)) {
-                    wordSet.add(word);
-                }
-            }
-        }
-
-        return wordSet;
-    }
-
-    private static Set<String> buildStopWordSet(String filepath) throws IOException {
-        Set<String> stopWords = new TreeSet<>();
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filepath), "UTF-8"));
-        String line;
-
-        while ((line = br.readLine()) != null) {
-            stopWords.add(line.trim().toLowerCase());
-        }
-
-        return stopWords;
-    }
-
-    private static List<Document> buildDocuments(String filepath, Set<String> stopWords) {
-        List<Document> documents = new ArrayList<>();
-
-        Path path = Paths.get(filepath);
-        Stream<Path> filePathStream;
-        try {
-            filePathStream = Files.walk(path);
-        } catch (IOException e) {
-            throw new IllegalArgumentException("Could not walk path " + path);
-        }
-
-        filePathStream.forEach(p -> {
-            if (Files.isRegularFile(p)) {
-                try {
-                    documents.add(new Document(p.toString(), stopWords));
-                } catch (IOException e) {
-                    System.out.println("Could not read file: " + p.toString());
-                    System.out.println("Going to next file.");
-                }
-            }
-        });
-
-        return documents;
-    }
-
-    private static Map<String, Double> buildIdf(List<Document> documents) {
-        int D = documents.size();
-        Map<String, Double> idf = new TreeMap<>();
-
-        for (String word : vocabulary) {
-            word = word.trim().toLowerCase();
-            double idfValue = Math.log(D * 1.0 / wordAppearance(word, documents));
-            idf.put(word, idfValue);
-        }
-
-        for (Document doc : documents) {
-            doc.setIdf(idf);
-        }
-
-        return idf;
-    }
-
-    private static int wordAppearance(String word, List<Document> documents) {
-        int count = 0;
-
-        for (Document doc : documents) {
-            if (doc.getTf().containsKey(word)) {
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-    private static double calculateSimilarity(Document doc1, Document doc2) {
-        double numerator = 0;
-        Document shorter = doc1.getTfidf().size() < doc2.getTfidf().size() ? doc1 : doc2;
-        Document longer = doc1.getTfidf().size() <= doc2.getTfidf().size() ? doc2 : doc1;
-        for (String word : shorter.getTfidf().keySet()) {
-            if (!longer.getTfidf().containsKey(word)) {
-                continue;
-            }
-
-            numerator += shorter.getTfidf().get(word) * longer.getTfidf().get(word);
-        }
-
-        double denominator = normTfidf(shorter.getTfidf()) * normTfidf(longer.getTfidf());
-
-        return numerator / denominator;
-    }
-
-    private static double normTfidf(Map<String, Double> tfidf) {
-        double sumSquares = 0;
-        for (String word : tfidf.keySet()) {
-            sumSquares += tfidf.get(word) * tfidf.get(word);
-        }
-
-        return Math.sqrt(sumSquares);
     }
 }
--- hw16/hw16-0036491377-01/src/main/java/hr/fer/zemris/java/hw16/trazilica/SearchEngine.java
+++ hw16/hw16-0036491377-01/src/main/java/hr/fer/zemris/java/hw16/trazilica/SearchEngine.java
@@ -4,23 +4,43 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.stream.Stream;
 
 public class SearchEngine {
     private final String STOPWORD_PATH = "stop_rijeci.txt";
 
     private Set<String> vocabulary;
     private Set<String> stopWords;
+    private List<Document> documents;
     private Map<String, Double> idf;
-    private Map<Document, Double> results;
+    private List<Result> results;
 
-    public SearchEngine(String docPath) {
+    public SearchEngine(String docPath) throws IOException {
         stopWords = buildStopWordSet(STOPWORD_PATH);
+        vocabulary = buildVocabulary(docPath);
+        documents = buildDocuments(docPath, stopWords);
     }
 
-    private static Set<String> buildStopWordSet(String filepath) throws IOException {
+    private static double normTfidf(Map<String, Double> tfidf) {
+        double sumSquares = 0;
+        for (String word : tfidf.keySet()) {
+            sumSquares += tfidf.get(word) * tfidf.get(word);
+        }
+
+        return Math.sqrt(sumSquares);
+    }
+
+    private Set<String> buildStopWordSet(String filepath) throws IOException {
         Set<String> stopWords = new TreeSet<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filepath), "UTF-8"));
         String line;
@@ -30,5 +50,178 @@ public class SearchEngine {
         }
 
         return stopWords;
+    }
+
+    private Set<String> buildVocabulary(String filepath) {
+        Path path = Paths.get(filepath);
+        Stream<Path> filePathStream;
+        Set<String> vocabulary = new TreeSet<>();
+
+        try {
+            filePathStream = Files.walk(path);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Could not walk path " + path);
+        }
+
+        filePathStream.forEach(p -> {
+            if (Files.isRegularFile(p)) {
+                try {
+                    vocabulary.addAll(extractWords(p));
+                } catch (IOException e) {
+                    System.out.println("Could not read file: " + p.toString());
+                    System.out.println("Going to next file.");
+                }
+            }
+        });
+
+        return vocabulary;
+    }
+
+    private Set<String> extractWords(Path path) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path.toString()), "UTF-8"));
+        Set<String> wordSet = new TreeSet<>();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] words = line.trim().split("[^\\p{L}]+");
+
+            for (String word : words) {
+                word = word.trim().toLowerCase();
+                if (!word.isEmpty() && !stopWords.contains(word)) {
+                    wordSet.add(word);
+                }
+            }
+        }
+
+        return wordSet;
+    }
+
+    private List<Document> buildDocuments(String filepath, Set<String> stopWords) {
+        List<Document> documents = new ArrayList<>();
+
+        Path path = Paths.get(filepath);
+        Stream<Path> filePathStream;
+        try {
+            filePathStream = Files.walk(path);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Could not walk path " + path);
+        }
+
+        filePathStream.forEach(p -> {
+            if (Files.isRegularFile(p)) {
+                try {
+                    documents.add(new Document(p.toString(), stopWords));
+                } catch (IOException e) {
+                    System.out.println("Could not read file: " + p.toString());
+                    System.out.println("Going to next file.");
+                }
+            }
+        });
+
+        return documents;
+    }
+
+    private Map<String, Double> buildIdf(List<Document> documents) {
+        int D = documents.size();
+        Map<String, Double> idf = new TreeMap<>();
+
+        for (String word : vocabulary) {
+            word = word.trim().toLowerCase();
+            double idfValue = Math.log(D * 1.0 / wordAppearance(word, documents));
+            idf.put(word, idfValue);
+        }
+
+        for (Document doc : documents) {
+            doc.setIdf(idf);
+        }
+
+        return idf;
+    }
+
+    private int wordAppearance(String word, List<Document> documents) {
+        int count = 0;
+
+        for (Document doc : documents) {
+            if (doc.getTf().containsKey(word)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private double calculateSimilarity(Document queryDoc, Document existingDoc) {
+        double numerator = 0;
+        Document shorter = queryDoc.getTfidf().size() < existingDoc.getTfidf().size() ? queryDoc : existingDoc;
+        Document longer = queryDoc.getTfidf().size() <= existingDoc.getTfidf().size() ? existingDoc : queryDoc;
+        for (String word : shorter.getTfidf().keySet()) {
+            if (!longer.getTfidf().containsKey(word)) {
+                continue;
+            }
+
+            numerator += shorter.getTfidf().get(word) * longer.getTfidf().get(word);
+        }
+
+        double denominator = normTfidf(shorter.getTfidf()) * normTfidf(longer.getTfidf());
+
+        return numerator / denominator;
+    }
+
+    public void calculateSimilaritiesTo(List<String> words) {
+        Document queryDoc = new Document(words, stopWords);
+        if (results == null) {
+            results = new ArrayList<>();
+        }
+
+        for (Document doc : documents) {
+            double sim = calculateSimilarity(queryDoc, doc);
+            results.add(new Result(doc, sim));
+        }
+
+        results.sort(Comparator.comparingDouble(Result::getSimilarity));
+    }
+
+    public List<Result> getResults() {
+        if (results == null) {
+            throw new IllegalStateException("Did not calculate similarities yet. Call calculateSimilaritiesTo first.");
+        }
+
+        return results;
+    }
+
+    public List<Result> getTopDocuments(int n) {
+        if (results == null) {
+            throw new IllegalStateException("Did not calculate similarities yet. Call calculateSimilaritiesTo first.");
+        }
+
+        List<Result> top = new ArrayList<>();
+        n = results.size() < n ? results.size() : n;
+        for (int i = 0; i < n; i++) {
+            Result r = results.get(i);
+            if (r.getSimilarity() <= 0) {
+                break;
+            }
+            top.add(r);
+        }
+
+        return top;
+    }
+
+    public static class Result {
+        private Document document;
+        private double similarity;
+
+        public Result(Document document, double similarity) {
+            this.document = document;
+            this.similarity = similarity;
+        }
+
+        public Document getDocument() {
+            return document;
+        }
+
+        public double getSimilarity() {
+            return similarity;
+        }
     }
 }
