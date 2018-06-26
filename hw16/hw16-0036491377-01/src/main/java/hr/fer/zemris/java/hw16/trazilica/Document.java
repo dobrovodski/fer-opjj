202f0af4a27bdda97cabbf0bda714b3391e27df5
Matej Dobrovodski <matej.dobrovodski@fer.hr>
2018-06-26 02:42:35 +0200

    Add texts. Vocabulary and stop-words building done

---
+++ hw16/hw16-0036491377-01/src/main/java/hr/fer/zemris/java/hw16/trazilica/Document.java
@@ -0,0 +1,17 @@
+package hr.fer.zemris.java.hw16.trazilica;
+
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Document {
+    private TreeMap<String, Double> vectors;
+    private String filepath;
+
+    public Document(String filepath) {
+        this.filepath = filepath;
+    }
+
+    private void buildVectors() {
+
+    }
+}
---
+++ hw16/hw16-0036491377-01/src/main/java/hr/fer/zemris/java/hw16/trazilica/Konzola.java
@@ -0,0 +1,116 @@
+package hr.fer.zemris.java.hw16.trazilica;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+
+public class Konzola {
+    private static final String PROMPT = "Enter command > ";
+    private static final String STOPWORD_PATH = "stop_rijeci.txt";
+    private static TreeSet<String> vocabulary;
+    private static TreeSet<String> stopWords;
+    private static double idf;
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("This program requires only the path to a folder of text documents.");
+            return;
+        }
+
+        String filepath = args[0];
+        try {
+            stopWords = buildStopWordSet(STOPWORD_PATH);
+        } catch (IOException e) {
+            System.out.println("Could not read stop word file: " + filepath);
+            return;
+        }
+
+        try {
+            vocabulary = buildVocabulary(filepath);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Could not build vocabulary: " + e.getMessage());
+            return;
+        }
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print(PROMPT);
+            String[] in = sc.nextLine().trim().split(" ", 2);
+            String command = in[0].trim();
+            if (command.equals("query")) {
+
+            } else if (command.equals("type")) {
+
+            } else if (command.equals("results")) {
+
+            } else if (command.equals("exit")) {
+                break;
+            } else {
+                System.out.println("Nepoznata naredba.");
+            }
+        }
+    }
+
+    private static TreeSet<String> buildVocabulary(String filepath) {
+        Path path = Paths.get(filepath);
+        Stream<Path> filePathStream;
+        TreeSet<String> vocabulary = new TreeSet<>();
+
+        try {
+            filePathStream = Files.walk(path);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Could not walk path " + path);
+        }
+
+        filePathStream.forEach(p -> {
+            if (Files.isRegularFile(p)) {
+                try {
+                    vocabulary.addAll(extractWords(p));
+                } catch (IOException e) {
+                    System.out.println("Could not read file: " + p.toString());
+                    System.out.println("Going to next file.");
+                }
+            }
+        });
+
+        return vocabulary;
+    }
+
+    private static TreeSet<String> extractWords(Path path) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path.toString()), "UTF-8"));
+        TreeSet<String> wordSet = new TreeSet<>();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] words = line.trim().split("[^\\p{L}]+");
+
+            for (String word : words) {
+                word = word.trim().toLowerCase();
+                if (!word.isEmpty() && !stopWords.contains(word)) {
+                    wordSet.add(word);
+                }
+            }
+        }
+
+        return wordSet;
+    }
+
+    private static TreeSet<String> buildStopWordSet(String filepath) throws IOException {
+        TreeSet<String> stopWords = new TreeSet<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filepath), "UTF-8"));
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            stopWords.add(line.trim().toLowerCase());
+        }
+
+        return stopWords;
+    }
+}
